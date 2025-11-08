386dae1daabfd172fb9738748acd10cee13b13bc
tomwalla917 <tywalla@gmail.com>
2025-11-08 10:54:32 -0500

    updated friendsBooks to use trending books cod and modal

--- src/context/FriendsBooks.jsx
+++ src/context/FriendsBooks.jsx
@@ -1,67 +1,82 @@
+import { useEffect, useState } from "react";
+import BookModal from "./BookModal";
 
+function FriendsBooks() {
+  const [books, setBooks] = useState([]);
+  const [selectedBook, setSelectedBook] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-function FriendsBooks () {
-    const books =[
-        {
-            id: 1, 
-            title: "The Song of Achilles",
-            author: "Madeline Miller",
-            coverUrl: "http://books.google.com/books/content?id=szMU9omwV0wC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  const titles = [
+    "The Song of Achilles",
+    "Between Two Fires",
+    "The Man in the High Castle",
+    "Frankenstein",
+    "We Share the Sun",
+    "When Breath Becomes Air"
+  ];
 
-        },
-        {
-            id: 2,
-            title: "Between Two Fires",
-            author: "Christopher Buehlman",
-            coverUrl: "http://books.google.com/books/content?id=YetvDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-        },
-        {
-            id: 3,
-            title: "The Man in the High Castle",
-            author: "Philip K. Dick",
-            coverUrl:"http://books.google.com/books/content?id=NtukmPtq2l8C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-        },
-        {
-            id: 4,
-            title: "Frankenstein",
-            author: "Mary Shelley",
-            coverUrl: "http://books.google.com/books/content?id=9xHCAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-        },
-        {
-            id: 5,
-            title: "We Share the Sun",
-            author: "Sarah Gearhart",
-            coverUrl: "http://books.google.com/books/content?id=qvN0EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-        },
-        {
-            id: 6,
-            title: "When Breath Becomes Air",
-            author: "Paul Kalanithi",
-            coverUrl: "http://books.google.com/books/content?id=Dg1sCQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
-        }
-    ];
+  useEffect(() => {
+    async function fetchBooks() {
+      const fetched = await Promise.all(
+        titles.map(async (title) => {
+          const res = await fetch(
+            `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(title)}`
+          );
+          const data = await res.json();
+          const info = data.items?.[0]?.volumeInfo;
 
-    return (
-        <div className="col-12">
-            <h2 className="section-title">Books Popular With Your Friends</h2>
-            <div className="book-grid">
-                {books.map((book) => (
-                    <div key={book.id} className="book-card">
-                        <img
-                            src={book.coverUrl}
-                            alt={`${book.title} cover`}
-                            className="book-cover"
-                        />
-                        <div className="book-info">
-                            <h5 className="book-title">{book.title}</h5>
-                            <p className="book-author">{book.author}</p>
-                        </div>
-                    </div>
-                ))}
+          return info
+            ? {
+                title: info.title,
+                author: info.authors?.[0] || "Unknown",
+                coverUrl: info.imageLinks?.thumbnail,
+                pageCount: info.pageCount || "N/A",
+              }
+            : null;
+        })
+      );
+
+      setBooks(fetched.filter(Boolean));
+    }
+
+    fetchBooks();
+  }, []);
+
+  const handleBookClick = (book) => {
+    setSelectedBook(book);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedBook(null);
+  };
+
+  return (
+    <div className="col-12">
+      <h2 className="section-title">Friends Books</h2>
+      <div className="book-grid">
+        {books.map((book, i) => (
+          <div 
+            key={i} 
+            className="book-card"
+            onClick={() => handleBookClick(book)}
+          >
+            <img src={book.coverUrl} alt={book.title} className="book-cover" />
+            <div className="book-info">
+              <h5>{book.title}</h5>
+              <p>{book.author}</p>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+      <BookModal 
+        book={selectedBook}
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+      />
+    </div>
+  );
 }
 
 export default FriendsBooks;
-
