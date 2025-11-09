940d7d36ac6ca7ad0c5cbbf738c8ca6f3c39812e
tomwalla917 <tywalla@gmail.com>
2025-11-09 07:39:24 -0500

    working on the modal

--- src/context/BookModal.jsx
+++ src/context/BookModal.jsx
@@ -1,4 +1,4 @@
-function BookModal({book, isOpen, onClose}) {
+function BookModal({ book, isOpen, onClose }) {
     if (!isOpen || !book) return null;
 
     return (
@@ -6,14 +6,20 @@ function BookModal({book, isOpen, onClose}) {
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <button className="modal-close" onClick={onClose}>&times;</button>
                 <div className="modal-body">
-                    <img src={book.coverUrl} alt={`${book.title} cover`} className="modal-cover"/>
-                    <div className="modal-info">
+                    <div className="modal-left-column">
+                        <img src={book.coverUrl} alt={`${book.title} cover`} className="modal-cover" />
+
                         <h2>{book.title}</h2>
                         <p className="modal-author">by {book.author}</p>
                         <p className="modal-pages">Pages: {book.pageCount}</p>
                         <p className="modal-genre">Genre: {book.categories}</p>
-                        <p className="modal-plot">Plot: {book.description}</p>
                     </div>
+                    <div className="modal-right-column">
+                        <p className="modal-plot">Plot: {book.description}</p>
+                        <button>Add to List</button>
+                        <button>Write Review</button>
+                    </div>
+
                 </div>
             </div>
         </div>
