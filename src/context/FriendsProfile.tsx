59f71d14e08f01f7b331a934aa6ff26b0cf2eabe
calebliljeros-hash <caleb.liljeros@gmail.com>
2025-11-13 17:55:43 -0500

    Enhance `FriendsProfile` to load friend data from `location.state` or fallback to `defaultUser.friends`; integrate `FriendProfilePage` for detailed display.

--- src/context/FriendsProfile.tsx
+++ src/context/FriendsProfile.tsx
@@ -1,10 +1,33 @@
-import { useParams } from "react-router-dom";
+import {useLocation, useParams} from "react-router-dom";
+import {User} from "../types/User";
+import * as React from "react";
+import {defaultUser} from "../types/User";
+import FriendProfilePage from "./FriendProfilePage";
+
+interface LocationState {
+    friend?: User;
+}
 
 function FriendsProfile() {
     const { username } = useParams();
+    const location = useLocation();
+    const state = location.state as LocationState | null;
+    const friendFromState = state?.friend;
+    const friend =
+        friendFromState ??
+        defaultUser.friends.find((f) => f.username === username);
+
+
+    if (!friend) {
+        return <p className="text-muted">Friend not found.</p>;
+    }
 
     return (
-        <h2 className="boxFriends mb-3 section-title">{username}'s page</h2>
+        <div>
+            <h2 className="boxFriends mb-3 section-title">{username}'s page</h2>
+
+            <FriendProfilePage user={friend} />
+        </div>
     );
 }
 export default FriendsProfile;
