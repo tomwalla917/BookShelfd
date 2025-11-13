d8df805c46650cb5a86c5481d5b1e25df649281f
calebliljeros-hash <caleb.liljeros@gmail.com>
2025-11-13 17:02:09 -0500

    New Friends Page, changed friends cards to be displayed in sidebar and profile to dynamically update when youve clicked on a friend.

---
+++ src/pages/Friends.tsx
@@ -0,0 +1,36 @@
+import { Link, Outlet } from "react-router-dom";
+import {friend1, friend2, friend3, friend4, friend5, User} from "../types/User.js";
+import FriendProfileCard from "../context/FriendProfileCard.js";
+
+
+const friends :User[] = [friend1, friend2, friend3, friend4, friend5];
+
+function Friends(){
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <aside className="col-md-4 col-lg-3 border-end py-3">
+                    <h2 className="boxFriends mb-3 section-title">Friends</h2>
+                    <ul className="list-unstyled mb-0">
+                        {friends.map((friend) => (
+                            <li key={friend.id} className="mb-3">
+                                <Link
+                                    to={friend.username}
+                                    className="text-decoration-none d-block"
+                                >
+                                    <FriendProfileCard friend={friend} />
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </aside>
+
+                <main className="col-md-8 col-lg-9 py-3">
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+}
+
+export default Friends
